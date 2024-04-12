4a817038d87e77d5b24759d34deb4eb763566c12
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-04-12 00:56:09 -0500

    feat: Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -9,3 +9,4 @@ let addPadding = question.parentElement.parentElement;
 addPadding.classList.toggle('questions__padding--add');
 question.children[0].classList.toggle('questions__arrow--rotate');
 if(answer.clientHeight === 0){
+    height = answer.scrollHeight;
