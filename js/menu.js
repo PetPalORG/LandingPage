762e10a57964299668329b5d8d68ee6d45dca521
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-04-12 00:51:39 -0500

    feat: Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,4 @@
 const openButton = document.querySelector('.nav__menu');
 const menu = document.querySelector('.nav__link');
   const closeMenu = document.querySelector('.nav__close');
+openButton.addEventListener('click', ()=>{
