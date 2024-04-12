97836e18f3bcf98d9857be4df41903483212aef5
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-04-12 00:55:55 -0500

    feat: Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -8,3 +8,4 @@ let answer = question.nextElementSibling;
 let addPadding = question.parentElement.parentElement;
 addPadding.classList.toggle('questions__padding--add');
 question.children[0].classList.toggle('questions__arrow--rotate');
+if(answer.clientHeight === 0){
