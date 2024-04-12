96447dc80a460df5ada10751f9382bc40179f1ba
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-04-12 00:59:11 -0500

    feat: Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -3,3 +3,4 @@ const sliders = [...document.querySelectorAll('.testimony__body')];
 const buttonNext = document.querySelector('#next');
 const buttonBefore = document.querySelector('#before');
 let value;   
+  buttonNext.addEventListener('click', ()=>{
