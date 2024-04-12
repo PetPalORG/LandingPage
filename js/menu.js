0bdc496f2ad604391ccf6dbe7f73c9723243f275
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-04-12 00:51:53 -0500

    feat: Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,4 @@ const openButton = document.querySelector('.nav__menu');
 const menu = document.querySelector('.nav__link');
   const closeMenu = document.querySelector('.nav__close');
 openButton.addEventListener('click', ()=>{
+menu.classList.add('nav__link--show');
